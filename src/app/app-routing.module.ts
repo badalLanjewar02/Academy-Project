8d673b804135a90e4229f547c0c9f51036885723
badalLanjewar02 <badallanjewar999@gmail.com>
2025-09-15 23:10:17 +0530

    Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeInfoComponent } from './Home/home-info/home-info.component';
+import { GalleryComponent } from './Gallery/gallery/gallery.component';
+import { PrimaryWingComponent } from './Admissions/admissions/primary-wing/primary-wing.component';
+
 
 const routes: Routes = [
-  // {
-  //   path: 'home',
-  //   component: HomeComponent,
-  //   children: [
-  //     { path: 'about', component: AboutComponent },
-  //     { path: 'images', component: ImagesComponent }
-  //   ]
-  // },
 
+ { path: 'PrimaryWing', component: PrimaryWingComponent },
   {
     path: '', component: HomeInfoComponent
+  },
+  {
+    path:'Gallery',component:GalleryComponent
   }
 ];
 
